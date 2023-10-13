7e5cd95f1954d28f6e9abdc3801f48845fc63d72
MR.KOKAS <88835939+KOKAS-3o14TOS@users.noreply.github.com>
2023-10-12 23:39:20 -0600

    Add files via upload

--- C/ACT3_opemb/exercise02.cpp
+++ C/ACT3_opemb/exercise02.cpp
@@ -1,14 +1,23 @@
+/*
+Desarrolla una función que regresa la suma de todos los números primos que hay entre 1 y 5,000,000 (cinco millones).
+
+Jorge Martínez Lopez A01704518
+Gaddiel Lara Roldán A01704231
+
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <chrono>
-#include <cmath>
-#include <cstring>
 #include <omp.h>
+#include <cmath>
 #include "utils.h"
 #define MAX 5000000
 
 using namespace std;
 using namespace std::chrono;
+
+long long suma;
 
 bool es_primo(int n){
 	if (n<2) return false;
@@ -18,35 +27,29 @@ bool es_primo(int n){
 	return true;
 }
 
-void sumar(int *array,int &suma,int max){
-    #pragma omp parallel for shared(array, suma, max)
-    for(int i=0;i<max;i++){
-		if (es_primo(array[i])){
-			suma+=i;
-		}
-	}
+long long sum(int n) {
+    long long s=0;
+    #pragma omp parallel for reduction(+:s)
+    for(int i=2;i<=n;i++) {
+        if (es_primo(i)) {
+            s+=i;
+        }
+    }
+    return s;
 }
 
 int main(int argc, char* argv[]) {
-	int *array;
-	int suma = 0;
-	////////////////
-	high_resolution_clock::time_point start, end;
-	double timeElapsed;
-	////////////////
-	array = new int[MAX];
-    fill_array(array,MAX);
-	display_array("array", array);
-	cout << "Starting...\n";
-	timeElapsed = 0;//////////////////////////////
-	for (int j = 0; j < N; j++) {
-		start = high_resolution_clock::now();/////////////////////////
-		sumar(array,suma,MAX);
-		end = high_resolution_clock::now();///////////////////////////
-		timeElapsed += duration<double, std::milli>(end - start).count();////////////////////////////
-	}
-	cout<<suma<<"\n";
-	cout << "avg time = " << fixed << setprecision(3) << (timeElapsed / N) <<  " ms\n";/////////////////////////////
-	delete [] array;
-	return 0;
+    high_resolution_clock::time_point start, end;
+    double timeElapsed;
+    cout << "Starting...\n";
+    timeElapsed = 0;
+    for (int j = 0; j < 1; j++) {
+        start = high_resolution_clock::now();
+        suma=sum(MAX);
+        end = high_resolution_clock::now();
+        timeElapsed += duration<double, std::milli>(end - start).count();
+    }
+    cout<<"suma: "<<suma<<"\n";
+    cout << "avg time = " << fixed << setprecision(3) << (timeElapsed / N) << " ms\n";
+    return 0;
 }
